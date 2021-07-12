e05d1e91285657de15f1f3505f991a04b08e8af9
lijolawrance <lijolawrance@gmail.com>
2021-07-12 12:48:44 +0530

    changes

---
+++ notebooks/DataAnalysis.py
@@ -0,0 +1,17 @@
+from notebooks.Database import Database
+from notebooks.Utilities import Utilities
+
+import pandas as pd
+import plotly.express as px
+from copy import copy
+from scipy import stats
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+
+
+class DataAnalysis:
+    def __init__(self):
+        self.db=Database()
+        self.ut=Utilities()
